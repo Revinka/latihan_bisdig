8a36f0afa936fde2ad5921f8816d8403f3610598
Revinka <e42241640@student.polije.ac.id>
2025-09-10 15:30:59 +0700

    sheyjava

---
+++ popup.js
@@ -0,0 +1,6 @@
+alert("Halo sayang");
+let konfirmasi = confirm ("aku boleh tanya ga?");
+if (konfirmasi) {
+    let should = prompt ("Gimana kabarmu hari ini?");
+    alert("Alhamdulillah, jangan lupa bersyukur ya...:*");
+}
